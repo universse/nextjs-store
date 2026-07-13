13fef55164fd97e517da6c8e1be669d676636ac5
universse <ychjiang1008@outlook.com>
2026-07-13 09:16:02 -0400

    Navbar:Logo, NavSearch, CartButton

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,5 +1,25 @@
-const CartButton = () => {
-  return <div>CartButton</div>;
-};
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { LuShoppingCart } from "react-icons/lu";
+
+async function CartButton() {
+  // temp
+  const numItemsInCart = 9;
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative"
+    >
+      <Link href="/cart">
+        <LuShoppingCart />
+      </Link>
+      <span className="absolute -top-3 -right-3 bg-primary text-xs text-white rounded-full h-6 w-6 flex items-center justify-center">
+        {numItemsInCart}
+      </span>
+    </Button>
+  );
+}
 
 export default CartButton;
--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { FaStore } from "react-icons/fa6";
+
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <Button size="icon">
+      <Link href="/">
+        <FaStore className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 };
 
 export default Logo;
