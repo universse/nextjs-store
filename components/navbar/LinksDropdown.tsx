1237f929570635abdb71b3115ee829e56df1571d
universse <ychjiang1008@outlook.com>
2026-07-14 22:35:31 -0400

    Navbar:LinksDropdown

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,5 +1,34 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { LuAlignLeft } from "react-icons/lu";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { links } from "@/utils/links";
+
 const LinksDropdown = () => {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        render={<Button variant="outline" className="flex gap-4 max-w-25" />}
+      >
+        <LuAlignLeft className="w-6 h-6" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40" align="start" sideOffset={10}>
+        {links.map((link) => (
+          <DropdownMenuItem key={link.href}>
+            <Link href={link.href} className="capitalize w-full">
+              {link.label}
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default LinksDropdown;
