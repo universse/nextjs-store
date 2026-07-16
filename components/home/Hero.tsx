a55e6feab5a04a9a95ced2b79ca310355da7c522
universse <ychjiang1008@outlook.com>
2026-07-16 04:46:03 -0400

    HomePage: Hero Section

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,5 +1,28 @@
+import Link from "next/link";
+import HeroCarousel from "./HeroCarousel";
+import { Button } from "@/components/ui/button";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          Everything you need, all in one place.
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+          Explore a curated collection of high-quality products designed to make
+          everyday life better. Shop effortlessly with a seamless experience
+          from browsing to checkout.Find the latest products, exclusive deals,
+          and everyday essentials with a simple and enjoyable shopping
+          experience built for you.
+        </p>
+        <Button size="lg" className="mt-10">
+          <Link href="/products">Shop Now</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 };
 
 export default Hero;
