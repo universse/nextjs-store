654ae96c71d3afb8adec8f933a08462c669aec03
universse <ychjiang1008@outlook.com>
2026-07-16 05:00:39 -0400

    Loading Container

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Hero from "@/components/home/Hero";
 import FeaturedProducts from "@/components/home/FeaturedProducts";
+import { Suspense } from "react";
+import LoadingContainer from "@/components/global/LoadingContainer";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      <FeaturedProducts />
+      <Suspense fallback={<LoadingContainer />}>
+        <FeaturedProducts />
+      </Suspense>
     </>
   );
 };
