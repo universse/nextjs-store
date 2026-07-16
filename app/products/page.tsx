92a5e1f65513a57c4516603475cdcfb44cef1ef0
universse <ychjiang1008@outlook.com>
2026-07-16 05:47:20 -0400

    Products Page: ProductsContainer Component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,14 @@
-const ProductsPage = () => {
-  return <div>ProductsPage</div>;
-};
+import ProductsContainer from "@/components/products/ProductsContainer";
+
+async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ layout?: string; search?: string }>;
+}) {
+  const layout = (await searchParams).layout || "grid";
+  const search = (await searchParams).search || "";
+
+  return <ProductsContainer layout={layout} search={search} />;
+}
 
 export default ProductsPage;
