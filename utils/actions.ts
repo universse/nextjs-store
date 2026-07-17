9e4081237f1b06ecbbd0faecdb4a68ef094069e5
universse <ychjiang1008@outlook.com>
2026-07-17 03:37:08 -0400

    NavSearch Component

--- utils/actions.ts
+++ utils/actions.ts
@@ -8,8 +8,14 @@ export const fetchFeaturedProducts = async () => {
   return products;
 };
 
-export const fetchAllProducts = () => {
+export const fetchAllProducts = ({ search = "" }: { search: string }) => {
   return db.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: search, mode: "insensitive" } },
+        { company: { contains: search, mode: "insensitive" } },
+      ],
+    },
     orderBy: { createdAt: "desc" },
   });
 };
