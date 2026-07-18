c7fb2e62591ebf5f3a2cadb41af9145f2ae8cfe0
universse <ychjiang1008@outlook.com>
2026-07-18 15:00:36 -0400

    Navbar: LinksDropdown Complete

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -9,6 +9,9 @@ import { LuAlignLeft } from "react-icons/lu";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { links } from "@/utils/links";
+import { Show, SignInButton, SignUpButton } from "@clerk/nextjs";
+import UserIcon from "./UserIcon";
+import SignOutLink from "./SignOutLink";
 
 const LinksDropdown = () => {
   return (
@@ -17,15 +20,34 @@ const LinksDropdown = () => {
         render={<Button variant="outline" className="flex gap-4 max-w-25" />}
       >
         <LuAlignLeft className="w-6 h-6" />
+        <UserIcon />
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-40" align="start" sideOffset={10}>
-        {links.map((link) => (
-          <DropdownMenuItem key={link.href}>
-            <Link href={link.href} className="capitalize w-full">
-              {link.label}
-            </Link>
+        <Show when="signed-out">
+          <DropdownMenuItem>
+            <SignInButton mode="modal">
+              <button className="w-full text-left">Login</button>
+            </SignInButton>
           </DropdownMenuItem>
-        ))}
+          <DropdownMenuItem>
+            <SignUpButton mode="modal">
+              <button className="w-full text-left">Register</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </Show>
+        <Show when="signed-in">
+          {links.map((link) => (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className="capitalize w-full">
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          ))}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignOutLink />
+          </DropdownMenuItem>
+        </Show>
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,5 +1,21 @@
+"use client";
+
+import { SignOutButton } from "@clerk/nextjs";
+import { toast } from "sonner";
+import Link from "next/link";
+
 const SignOutLink = () => {
-  return <div>SignOutLink</div>;
+  const handleLogout = () => {
+    toast("Logout Successful");
+  };
+
+  return (
+    <SignOutButton>
+      <Link href="/" className="w-full text-left" onClick={handleLogout}>
+        Logout
+      </Link>
+    </SignOutButton>
+  );
 };
 
 export default SignOutLink;
