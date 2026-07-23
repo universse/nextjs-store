902ca028a3d2fe5b0c4ca5a47bc7b704e4598a98
universse <ychjiang1008@outlook.com>
2026-07-23 10:31:38 -0400

    Edit Product Page

---
+++ components/form/ImageInputContainer.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import FormContainer from "./FormContainer";
+import ImageInput from "./ImageInput";
+import { SubmitButton } from "./Buttons";
+import { FormAction } from "@/types/actions";
+
+type ImageInputContainerProps = {
+  image: string;
+  name: string;
+  action: FormAction;
+  text: string;
+  children?: React.ReactNode;
+};
+
+function ImageInputContainer(props: ImageInputContainerProps) {
+  const { image, name, action, text } = props;
+  const [isUpdateFormVisible, setUpdateFormVisible] = useState(false);
+
+  return (
+    <div className="mb-8">
+      <Image
+        src={image}
+        width={200}
+        height={200}
+        className="rounded-md object-cover mb-4 w-50 h-50"
+        alt={name}
+        preload
+      />
+
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={() => setUpdateFormVisible((prev) => !prev)}
+      >
+        {text}
+      </Button>
+      {isUpdateFormVisible && (
+        <div className="max-w-md mt-4">
+          <FormContainer action={action}>
+            {props.children}
+            <ImageInput />
+            <SubmitButton size="sm" text={text} />
+          </FormContainer>
+        </div>
+      )}
+    </div>
+  );
+}
+export default ImageInputContainer;
--- utils/actions.ts
+++ utils/actions.ts
@@ -116,3 +116,70 @@ export const deleteProductAction = async (prevState: { productId: string }) => {
     return renderError(error);
   }
 };
+
+export const fetchAdminProductDetails = async (productId: string) => {
+  await getAdminUser();
+  const product = await db.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+  if (!product) redirect("/admin/products");
+
+  return product;
+};
+
+export const updateProductAction = async (
+  prevState: any,
+  formData: FormData,
+) => {
+  await getAdminUser();
+  try {
+    const productId = formData.get("id") as string;
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = validateWithZodSchema(productSchema, rawData);
+
+    await db.product.update({
+      where: {
+        id: productId,
+      },
+      data: {
+        ...validatedFields,
+      },
+    });
+
+    revalidatePath(`/admin/products/${productId}/edit`);
+    return { message: "Product updated successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
+
+export const updateProductImageAction = async (
+  prevState: any,
+  formData: FormData,
+) => {
+  await getAdminUser();
+  try {
+    const image = formData.get("image") as File;
+    const productId = formData.get("id") as string;
+    const oldImageUrl = formData.get("url") as string;
+
+    const validatedFile = validateWithZodSchema(imageSchema, { image });
+    const fullPath = await uploadImage(validatedFile.image);
+    await deleteImage(oldImageUrl);
+    await db.product.update({
+      where: {
+        id: productId,
+      },
+      data: {
+        image: fullPath,
+      },
+    });
+
+    revalidatePath(`/admin/products/${productId}/edit`);
+    return { message: "Product Image updated successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
